e09b0768746e85e5fa21916b534845c18a542780
FDSnail <wukeng1122@126.com>
2018-10-24 19:51:34 +0800

    创建C基础工程

---
+++ C基础/C基础/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C基础
+//
+//  Created by wukeng on 2018/10/24.
+//  Copyright © 2018年 吴铿. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
